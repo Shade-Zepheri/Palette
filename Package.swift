100a014d8f318308b2c2510b5c46955611f204c1
Shade-Zepheri <ziroalpha@gmail.com>
2020-06-18 21:48:23 -0500

    [Package] Decrease tvOS version requirement

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v10),
         .macOS(.v10_12),
-        .tvOS(.v11)
+        .tvOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
